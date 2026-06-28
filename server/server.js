53d55558747913b33d92c8b7ce1c33ba7bc653a4
Surendar | Software Developer <shareknowledgeandfact@gmail.com>
2026-06-28 19:44:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:5000`);
+  console.log(`Server running on port ${PORT}`);
 });
